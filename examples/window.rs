61387b922938820c9f27913c7d374c03b88c553d
Jason Tsai <jason@pews.dev>
2022-03-25 23:45:13 +0800

    move menu build to function in example

--- examples/window.rs
+++ examples/window.rs
@@ -22,6 +22,34 @@ fn main() {
     .build(&event_loop)
     .unwrap();
 
+  let menu_bar = menu_bartender();
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    let channel = get_event_channel();
+    let rx_ref = channel.1.clone();
+
+    /* recv menu events */
+    while let Ok(data) = rx_ref.try_recv() {
+      println!("{:?}", data);
+    }
+
+    match event {
+      Event::NewEvents(winit::event::StartCause::Init) => set_menu(&menu_bar),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        window_id,
+      } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+      Event::MainEventsCleared => {
+        window.request_redraw();
+      }
+      _ => (),
+    }
+  });
+}
+
+fn menu_bartender() -> Menu {
   /* menu bar */
   let menu_bar: Menu = Menu::new();
 
@@ -72,27 +100,5 @@ fn main() {
 
   menu_bar.add_submenu(&first_menu, "First Menu");
 
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
-
-    let channel = get_event_channel();
-    let rx_ref = channel.1.clone();
-
-    /* recv menu events */
-    while let Ok(data) = rx_ref.try_recv() {
-      println!("{:?}", data);
-    }
-
-    match event {
-      Event::NewEvents(winit::event::StartCause::Init) => set_menu(&menu_bar),
-      Event::WindowEvent {
-        event: WindowEvent::CloseRequested,
-        window_id,
-      } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-      Event::MainEventsCleared => {
-        window.request_redraw();
-      }
-      _ => (),
-    }
-  });
+  menu_bar
 }
